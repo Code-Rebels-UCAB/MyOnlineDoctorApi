c78a324f52cd7ca559d04bbbbc7611d115e7be81
Carlos Landaeta <calejandrolg99@gmail.com>
2022-07-12 19:17:23 -0400

    Value Objects Faltantes e instalacion de GUID

---
+++ src/paciente/dominio/values/PacienteID.ts
@@ -0,0 +1,17 @@
+import { IValueObject } from 'src/commun/dominio/values/IValueObject';
+import { Guid } from 'guid-typescript';
+
+export class PacienteID implements IValueObject {
+  private readonly id: Guid;
+  constructor() {
+    this.id = Guid.create();
+  }
+
+  public getPacienteID(): Guid {
+    return this.id;
+  }
+
+  public esIgual(id: PacienteID): boolean {
+    return this.id == id.getPacienteID();
+  }
+}
