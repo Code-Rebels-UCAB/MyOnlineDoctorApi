48e89d241f85042afb6013232b5f5ecbcbf85b4f
Carlos Arriaga <arriaga1410@gmail.com>
2022-07-12 23:11:12 -0400

    Arreglando el conflicto de Paciente ID

--- src/commun/dominio/values/PacienteID.ts
+++ src/commun/dominio/values/PacienteID.ts
@@ -1,30 +1,10 @@
-<<<<<<< HEAD
-
-import { Guid } from 'guid-typescript';
-import { IValueObject } from 'interfaceVO';
-=======
 import { IValueObject } from "interfaceVO";
 import { Guid } from "guid-typescript";
 
->>>>>>> main
 
 export class PacienteID implements IValueObject {
   private readonly id: Guid;
 
-<<<<<<< HEAD
-  constructor() {
-    this.id = Guid.create();
-  }
-
-  public getPacienteID(): Guid {
-    return this.id;
-  }
-
-  public esIgual(id: PacienteID): boolean {
-    return this.id == id.getPacienteID();
-  }
-}
-=======
   private constructor() {
     this.id = Guid.create();
   }
@@ -37,4 +17,3 @@ export class PacienteID implements IValueObject {
     return this.id == pacienteID.getPacienteID();
   }
 }
->>>>>>> main
