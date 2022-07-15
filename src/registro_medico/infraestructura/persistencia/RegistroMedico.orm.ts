2ec910acfdba23cb0ece63928ad24e0795c3995e
Carlos Landaeta <calejandrolg99@gmail.com>
2022-07-14 20:56:56 -0400

    persistencia registroMedico lista

---
+++ src/registro_medico/infraestructura/persistencia/RegistroMedico.orm.ts
@@ -0,0 +1,51 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+import { DoctorORM } from '../../../doctor/infraestructura/persistencia/Doctor.orm';
+import { CitaORM } from '../../../cita/infraestructura/persistencia/Cita.orm';
+
+@Entity({ name: 'Registros_Medicos' })
+export class RegistroMedicoORM {
+  @PrimaryColumn()
+  id_registro: string;
+
+  @Column()
+  examenes: string;
+
+  @Column()
+  historia: string;
+
+  @Column()
+  prescripcion: string;
+
+  @Column()
+  plan: string;
+
+  @Column()
+  diagonistico: string;
+
+  @Column()
+  motivo: string;
+
+  @Column()
+  fechaCita: Date;
+
+  @OneToOne(() => DoctorORM)
+  @JoinColumn({ name: 'id_doctor' })
+  doctor: DoctorORM;
+
+  @OneToOne(() => CitaORM)
+  @JoinColumn({ name: 'id_cita' })
+  cita: CitaORM;
+
+  @ManyToOne(
+    () => HistoriaMedicaORM,
+    (HistoriaMedica) => HistoriaMedica.id_registroMedicos,
+  )
+  historia: string;
+}
