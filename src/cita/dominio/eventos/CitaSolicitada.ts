81936ae3e87a7e053ee96e262718ed1e880a4cd2
Joshep27 <flowersjoshep@gmail.com>
2022-07-15 11:12:17 -0600

    Un detalle en el Evento CitaSolicitada

--- src/cita/dominio/eventos/CitaSolicitada.ts
+++ src/cita/dominio/eventos/CitaSolicitada.ts
@@ -8,6 +8,7 @@ export class CitaSolicitada extends EventoDominio{
         private readonly IDDoctor:string,
         private readonly IDPaciente:string,
         private readonly IDCita:string,
+        private readonly Status:string,
         private readonly Motivo:string,
         private readonly Modalidad: string
     ){
@@ -17,10 +18,12 @@ export class CitaSolicitada extends EventoDominio{
 
     GetInformacion(): {} {
         return {
+            nombre_evento: this.Nombre,
+            fecha_generacion: this.Fecha,
             id_doctor: this.IDDoctor,
             id_paciente: this.IDPaciente,
             id_cita: this.IDCita,
-            status: 'Solicitada',
+            status: this.Status,
             motivo: this.Motivo,
             modalidad: this.Modalidad,
         }
