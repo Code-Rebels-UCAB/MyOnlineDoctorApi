79fde3d5f5c74d6a0eb644df3e4136fa84dd706d
Carlos Arriaga <arriaga1410@gmail.com>
2022-07-15 21:27:11 -0400

    Caso de uso de buscar doctor especialidad

---
+++ src/doctor/infraestructura/adaptadores/RepositorioDoctor.ts
@@ -0,0 +1,50 @@
+import { IRepositorioDoctor } from '../../aplicacion/puertos/IRepositorioDoctor';
+import { Repository } from 'typeorm'
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DoctorORM } from '../persistencia/Doctor.orm';
+
+
+@Injectable()
+export class RepositorioDoctor implements IRepositorioDoctor {
+
+    constructor(
+        @InjectRepository(DoctorORM)
+        private readonly _doctorRepository: Repository<DoctorORM>
+    ) { }
+    
+
+    async obtenerDoctorByEspecialidad(especialidad: string) {
+        const doctoresFiltrados =  await this._doctorRepository.createQueryBuilder('doctores')
+                                        .leftJoin('doctores.especialidades', 'especialidades')
+                                        .leftJoinAndSelect('doctores.especialidades', 'EspecialidadesSelect')
+                                        .where('especialidades.nombre like :especialidad', { especialidad: `%${especialidad}%`})
+                                        .orderBy('doctores.id_doctor', 'ASC')
+                                        .getMany();
+
+        const result =  await this._doctorRepository.createQueryBuilder('doctores')
+                                .leftJoin('doctores.especialidades', 'especialidades')
+                                .leftJoinAndSelect('doctores.especialidades', 'EspecialidadesSelect')
+                                .where('especialidades.nombre like :especialidad', { especialidad: `%${especialidad}%`})
+                                .orderBy('doctores.id_doctor', 'ASC').printSql()
+                                .getMany();
+
+        console.log(result);
+
+        return doctoresFiltrados;
+    }
+    
+    obtenerDoctorByNombre(nombre: string) {
+        throw new Error('Method not implemented.');
+    }
+    obtenerTopDoctores() {
+        throw new Error('Method not implemented.');
+    }
+    bloquearDoctor(id: string) {
+        throw new Error('Method not implemented.');
+    }
+    calificarDoctor(id: string, calificacion: number) {
+        throw new Error('Method not implemented.');
+    }
+  
+}
---
+++ src/doctor/infraestructura/api/doctor.module.ts
@@ -0,0 +1,38 @@
+import { DynamicModule,Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from '../../../commun/infraestructura/logger/logger.module';
+import { LoggerService } from '../../../commun/infraestructura/logger/logger.service';
+import { BuscarDoctorEspecialidad } from '../../aplicacion/servicios/BuscarDoctorEspecialidad';
+import { RepositorioDoctor } from '../adaptadores/RepositorioDoctor';
+import { DoctorORM } from '../persistencia/Doctor.orm';
+import { DoctorController } from './doctor.controller';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([DoctorORM ]),
+    LoggerModule,
+    
+  ],
+  controllers: [DoctorController],
+  providers: [BuscarDoctorEspecialidad,RepositorioDoctor, LoggerService],
+})
+export class DoctorModule {
+  static register(): DynamicModule {
+    return {
+      module: DoctorModule,
+      providers: [
+        {
+          inject: [LoggerService, RepositorioDoctor],
+          provide: BuscarDoctorEspecialidad,
+          useFactory: (
+            logger: LoggerService,
+            userRepo: RepositorioDoctor,
+          ) => new BuscarDoctorEspecialidad(logger, userRepo),
+        },
+      ],
+    };
+  }
+}
+
