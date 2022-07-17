972b254af8572627e6974b4af38ff7c02779147f
Christian Jose <christianjsanchezr2025@gmail.com>
2022-07-16 22:04:12 -0400

    dto cita aplicacion

---
+++ src/cita/aplicacion/dto/CitaDataDTO.ts
@@ -0,0 +1,11 @@
+export interface CitaDataDTO {
+    idCita: string;
+    idPaciente: string;
+    idDoctor: string;
+    status: string;
+    modalidad: string;
+    motivo: string;
+    fechaCita: Date;
+    horaCita: string;
+    duracion: string;
+}
