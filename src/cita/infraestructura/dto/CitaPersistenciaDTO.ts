70a19afb652103d153ca6247c5c50d7d9aa37b7b
Joshep27 <flowersjoshep@gmail.com>
2022-07-16 22:25:32 -0600

    creación CitaPersistenciaDTO

---
+++ src/cita/infraestructura/dto/CitaPersistenciaDTO.ts
@@ -0,0 +1,15 @@
+import { RegistroMedicoORM } from "src/registro_medico/infraestructura/persistencia/RegistroMedico.orm";
+
+export interface CitaPersistenciaDTO{
+
+    id_cita: string;
+    statuscita: string;
+    modalidad: string;
+    motivo: string;
+    fechacita: Date;
+    horacita: Date;
+    duracion: number;
+    paciente: string;
+    doctor: string;
+    registroMedico: RegistroMedicoORM[];
+}
