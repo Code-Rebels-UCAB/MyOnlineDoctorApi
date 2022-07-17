0943f872f3d1a9ab5d10eba7cf7577a497d42178
Carlos Landaeta <calejandrolg99@gmail.com>
2022-07-16 22:12:24 -0400

    adaptador cita

---
+++ src/cita/infraestructura/adaptadores/RepositorioCita.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IRepositorioCita } from '../../aplicacion/puertos/IRepositorioCita';
+import { CitaORM } from '../persistencia/Cita.orm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RepositorioCita implements IRepositorioCita {
+  constructor(
+    @InjectRepository(CitaORM)
+    private readonly RepositorioCita: Repository<CitaORM>,
+  ) {}
+
+  obtenerTodasLasCitas() {
+    throw new Error('Method not implemented');
+  }
+
+  obtenerCitaById(id_cita: string) {
+    throw new Error('Method not implemented');
+  }
+
+  obtenerCitaByDoctor(id_doctor: string) {
+    throw new Error('Method not implemented');
+  }
+
+  obtenerCitaByPaciente(id_paciente: string) {
+    throw new Error('Method not implemented');
+  }
+
+  obtenerCitaByFecha(fecha: string) {
+    throw new Error('Method not implemented');
+  }
+
+  crearCita() {
+    throw new Error('Method not implemented');
+  }
+
+  actualizarStatusCita() {
+    throw new Error('Method not implemented');
+  }
+}
