18a21d32ae884e3cfba952bd84bac6b11095842c
Christian Jose <christianjsanchezr2025@gmail.com>
2022-07-20 16:01:53 -0400

    creando interceptor logger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './commun/infraestructura/excepciones/Excepcion.middleware';
+import { LoggingInterceptor } from './commun/infraestructura/logger/logger.interceptor';
 import { LoggerService } from './commun/infraestructura/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
   await app.listen(3000);
 }
 bootstrap();
