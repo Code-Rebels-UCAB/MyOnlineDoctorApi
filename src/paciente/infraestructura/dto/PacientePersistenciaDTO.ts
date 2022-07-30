2c3f15b69378a62467f9c943df322aa94c94ee85
Christian Jose <christianjsanchezr2025@gmail.com>
2022-07-29 20:47:06 -0400

    Creando CU BuscarPacienteNombre

---
+++ src/paciente/infraestructura/dto/PacientePersistenciaDTO.ts
@@ -0,0 +1,18 @@
+export interface PacientePersistenciaDTO {
+    id_paciente: string;
+    p_nombre: string;
+    s_nombre?: string;
+    p_apellido: string;
+    s_apellido?: string;
+    fecha_nacimiento: string;
+    telefono: string;
+    correo: string;
+    sexo: string;
+    altura: string;
+    peso: string;
+    contrasena: string;
+    status_suscripcion: string;
+    alergia?: string;
+    antecedentes?: string;
+    operacion?: string;
+}
