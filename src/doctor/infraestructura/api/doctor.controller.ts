5b68f40520e2eddd1aab6ca7d7a2270185a14d6e
ilixna <ilianadias9@gmail.com>
2022-08-03 23:42:28 -0400

    Cambios en los controladores cita y registro medico

--- src/doctor/infraestructura/api/doctor.controller.ts
+++ src/doctor/infraestructura/api/doctor.controller.ts
@@ -76,7 +76,9 @@ export class DoctorController {
 
   @Put('autenticar')
   async autenticar(@Body() data: AutenticarDoctorDTO) {
+    console.log(data);
     const response = await this.autenticarDoctor.ejecutar(data);
+    console.log(response);
     return response;
   }
 
