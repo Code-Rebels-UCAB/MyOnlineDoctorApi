0495abf037f87930c4fd2f7f414816baba48c801
Carlos Landaeta <calejandrolg99@gmail.com>
2022-08-03 20:45:07 -0400

    caso de prueba cantidad citas del dia

--- src/cita/infraestructura/adaptadores/__mocks__/RepositorioCita.ts
+++ src/cita/infraestructura/adaptadores/__mocks__/RepositorioCita.ts
@@ -1,96 +1,112 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { IRepositorioCita } from "../../../aplicacion/puertos/IRepositorioCita";
-import { Repository } from "typeorm";
-import { DoctorORM } from "../../../../doctor/infraestructura/persistencia/Doctor.orm";
-import { PacienteORM } from "../../../../paciente/infraestructura/persistencia/Paciente.orm";
-import { CitaORM } from "../../persistencia/Cita.orm";
-import { SolicitarCitaPersistenciaDTO } from "../../persistencia/SolicitarCitaPersistenciaDTO";
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { IRepositorioCita } from '../../../aplicacion/puertos/IRepositorioCita';
+import { Repository } from 'typeorm';
+import { DoctorORM } from '../../../../doctor/infraestructura/persistencia/Doctor.orm';
+import { PacienteORM } from '../../../../paciente/infraestructura/persistencia/Paciente.orm';
+import { CitaORM } from '../../persistencia/Cita.orm';
+import { SolicitarCitaPersistenciaDTO } from '../../persistencia/SolicitarCitaPersistenciaDTO';
 
 const Citas = [
-    {
-      id_cita: 'a42253d2-69bd-4136-8ee4-62ce9f84ea31',
-      statuscita: 'Solicitada',
-      modalidad: 'Virtual',
-      motivo:'Dolor de Cabello',
-      fechacita: null,
-      horacita: null,
-      duracion: null,
-      doctor:'0462d106-df70-4f8d-b322-0c736f4069e7',
-      paciente: 'ed649257-8091-4b77-827a-8532b5c4c826',
-
-    },
-]
-
+  {
+    id_cita: 'a42253d2-69bd-4136-8ee4-62ce9f84ea31',
+    statuscita: 'Solicitada',
+    modalidad: 'Virtual',
+    motivo: 'Dolor de Cabello',
+    fechacita: null,
+    horacita: null,
+    duracion: null,
+    doctor: '0462d106-df70-4f8d-b322-0c736f4069e7',
+    paciente: 'ed649257-8091-4b77-827a-8532b5c4c826',
+  },
+  {
+    id_cita: '0d8d0a1e-742b-476e-93f9-aaab325fc1c6',
+    statuscita: 'Agendada',
+    modalidad: 'Virtual',
+    motivo: 'Dolor de Cabello',
+    fechacita: new Date().toISOString().split('T')[0],
+    horacita: null,
+    duracion: null,
+    doctor: '0c8aa290-d11c-4d47-8e38-6606a03f434a',
+    paciente: 'ed649257-8091-4b77-827a-8532b5c4c826',
+  },
+];
 
 export class RepositorioCita implements IRepositorioCita {
-    constructor(
-      @InjectRepository(CitaORM)
-      private readonly RepositorioCita: Repository<CitaORM>,
-      @InjectRepository(DoctorORM)
-      private readonly RepositorioDoctor: Repository<DoctorORM>,
-      @InjectRepository(PacienteORM)
-      private readonly RepositorioPaciente: Repository<PacienteORM>,
-    ) {}
+  constructor(
+    @InjectRepository(CitaORM)
+    private readonly RepositorioCita: Repository<CitaORM>,
+    @InjectRepository(DoctorORM)
+    private readonly RepositorioDoctor: Repository<DoctorORM>,
+    @InjectRepository(PacienteORM)
+    private readonly RepositorioPaciente: Repository<PacienteORM>,
+  ) {}
 
-    obtenerTodasLasCitas() {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaById(id_cita: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaByDoctor(id_doctor: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaByPaciente(id_paciente: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaByFecha(fecha: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaDeDoctorByStatus(status: string, doctorid: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitasDeDoctor(doctorid: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitasDelDiaDoctor(doctorId: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCantidadPacientesPorDoctor(doctorId: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCantidadCitasDelDiaDoctor(doctorId: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerCitaIniciada(citaid: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerTokenF(citaid: string) {
-        throw new Error("Method not implemented.");
-    }
-    obtenerDoctorCita(citaid: string) {
-        throw new Error("Method not implemented.");
-    }
-    crearCita(cita: SolicitarCitaPersistenciaDTO) {
-        throw new Error("Method not implemented.");
-    }
-    actualizarStatusCita(citaid: string, status: string) {
-        throw new Error("Method not implemented.");
-    }
-    async actualizarCitaAgendada(citaid: string, fecha: string, hora: string, duracion: string) {
-        Citas.forEach( (cita) => {
-            if (cita.id_cita == citaid) {
-                cita.fechacita = fecha;
-                cita.horacita = hora;
-                cita.duracion = duracion;
-            }
-            });
-    }
-    actualizarDatosVideollamadaCita(citaid: string, nombreCanal: string, tokenCita: string) {
-        throw new Error("Method not implemented.");
-    }
-
-
-    
-    }
+  obtenerTodasLasCitas() {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitaById(id_cita: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitaByDoctor(id_doctor: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitaByPaciente(id_paciente: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitaByFecha(fecha: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitaDeDoctorByStatus(status: string, doctorid: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitasDeDoctor(doctorid: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCitasDelDiaDoctor(doctorId: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCantidadPacientesPorDoctor(doctorId: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerCantidadCitasDelDiaDoctor(doctorId: string) {
+    return Citas.filter(
+      (cita) => cita.fechacita == new Date().toISOString().split('T')[0],
+    ).length;
+  }
+  obtenerCitaIniciada(citaid: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerTokenF(citaid: string) {
+    throw new Error('Method not implemented.');
+  }
+  obtenerDoctorCita(citaid: string) {
+    throw new Error('Method not implemented.');
+  }
+  crearCita(cita: SolicitarCitaPersistenciaDTO) {
+    throw new Error('Method not implemented.');
+  }
+  actualizarStatusCita(citaid: string, status: string) {
+    throw new Error('Method not implemented.');
+  }
+  async actualizarCitaAgendada(
+    citaid: string,
+    fecha: string,
+    hora: string,
+    duracion: string,
+  ) {
+    Citas.forEach((cita) => {
+      if (cita.id_cita == citaid) {
+        cita.fechacita = fecha;
+        cita.horacita = hora;
+        cita.duracion = duracion;
+      }
+    });
+  }
+  actualizarDatosVideollamadaCita(
+    citaid: string,
+    nombreCanal: string,
+    tokenCita: string,
+  ) {
+    throw new Error('Method not implemented.');
+  }
+}
--- test/Doctor/unitarios/CantidadesDoctor.unit.spec.ts
+++ test/Doctor/unitarios/CantidadesDoctor.unit.spec.ts
@@ -1,29 +1,47 @@
 import { BuscarCantidadTodosLosPacientes } from '../../../src/paciente/aplicacion/servicios/BuscarCantidadPacientesSistema.service';
+import { CantidadCitasDiaDoctor } from '../../../src/cita/aplicacion/servicios/CantidadCitasDiaDoctor.service';
 import { LoggerMockFactory } from '../../fabricas/LoggerMockFactory';
 import { PacienteRepoMockFactory } from '../../fabricas/PacienteRepoMockFactory';
+import { CitaRepoMockFactory } from '../../fabricas/CitaRepoMockFactory';
 
 describe('Unitario - CU Cantidad Pacientes del Sistema, Cantidad de Citas del Dia de Doctor, Cantidad de Pacientes de Doctor', () => {
   let mockLoggerService;
   let mockRepositorioPaciente;
   let mockRepositorioCita;
-  let casoUso: BuscarCantidadTodosLosPacientes;
+  let casoPruebaTodosPacientes: BuscarCantidadTodosLosPacientes;
+  let casoPruebaCantidadCitasDia: CantidadCitasDiaDoctor;
 
   beforeAll(() => {
     mockLoggerService = LoggerMockFactory.Crear();
     mockRepositorioPaciente = PacienteRepoMockFactory.Crear();
-    // mockRepositorioCita = CitaRepoMockFactory.Crear();
-    casoUso = new BuscarCantidadTodosLosPacientes(
+    mockRepositorioCita = CitaRepoMockFactory.Crear();
+    casoPruebaTodosPacientes = new BuscarCantidadTodosLosPacientes(
       mockLoggerService,
       mockRepositorioPaciente,
     );
+    casoPruebaCantidadCitasDia = new CantidadCitasDiaDoctor(
+      mockLoggerService,
+      mockRepositorioCita,
+    );
   });
 
-  it('Debe devolver Cantidad de Pacientes Registrados en el Sistema', () => {
-    const resultado = casoUso.ejecutar();
+  it('Debe devolver la Cantidad de Pacientes Registrados en el Sistema', () => {
+    const resultado = casoPruebaTodosPacientes.ejecutar();
 
     return resultado.then((result) => {
       expect(result.esExitoso).toBeTruthy();
       expect(result.valor).toBe(3);
+    });
+  });
+
+  it('Debe devolver la Cantidad de Citas del Dia del Doctor', () => {
+    const resultado = casoPruebaCantidadCitasDia.ejecutar(
+      '0c8aa290-d11c-4d47-8e38-6606a03f434a',
+    );
+
+    return resultado.then((result) => {
+      expect(result.esExitoso).toBeTruthy();
+      expect(result.valor).toBe(1);
     });
   });
 });
